0182643e873983bcf90b713cf0c9ae8002d1caa7
Natalia Giraldo <nataliag0378@gmail.com>
2024-08-07 11:28:47 -0500

    feat: :sparkles: login finalizado

--- js/auth.js
+++ js/auth.js
@@ -2,19 +2,43 @@ document.getElementById('loginButton').addEventListener('click', async () => {
     const email = document.getElementById('floatingInput').value;
     const password = document.getElementById('floatingPassword').value;
 
-    const response = await fetch('http://localhost:8080/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ username: email, password: password })
-    });
+    try {
+        const response = await fetch('http://localhost:8080/auth/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ username: email, password: password })
+        });
 
-    if (response.ok) {
-        const token = response.headers.get('Authorization').replace('Bearer ', '');
-        localStorage.setItem('token', token);
-        alert('Login successful');
-    } else {
-        alert('Login failed');
+        if (response.ok) {
+            const data = await response.json();
+            const token = data.token;
+            localStorage.setItem('token', 'Bearer ' + token); 
+            alert('Login successful');
+            window.location.href = "/index.html";
+        } else {
+            const error = await response.json();
+            alert(`Login failed: ${error.message}`);
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        alert('An unexpected error occurred.');
     }
 });
+
+
+async function makeAuthenticatedRequest(url, options = {}) {
+    const token = localStorage.getItem('token');
+    if (!options.headers) {
+        options.headers = {};
+    }
+    if (token) {
+        options.headers['Authorization'] = token; 
+    }
+
+    const response = await fetch(url, options);
+    return response;
+}
+
+
