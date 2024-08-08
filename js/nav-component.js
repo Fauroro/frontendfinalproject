8c2d191d30c66de44d615c29392a4360244f55d5
Natalia Giraldo <nataliag0378@gmail.com>
2024-08-08 08:26:35 -0500

    feat: :white_check_mark: logout

--- js/nav-component.js
+++ js/nav-component.js
@@ -6,27 +6,41 @@ export class NavComponent extends HTMLElement {
 
     connectedCallback() {
         this.render();
+        this.addLogoutEventListener();
     }
 
     render() {
         this.innerHTML = /*html*/`
-		<nav class="nav-ppal">
-			<i class='bx bx-menu toggle-sidebar'></i>
-			<form action="#">
-
-			</form>
-
-			<span class="divider"></span>
-			<div class="profile">
-				<img
-					src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cGVvcGxlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-					alt="">
-				<ul class="profile-link">
-					<li><a href="#"><i class='bx bxs-log-out-circle'></i> Logout</a></li>
-				</ul>
-			</div>
-		</nav>
+        <nav class="nav-ppal">
+            <i class='bx bx-menu toggle-sidebar'></i>
+            <form action="#"></form>
+            <span class="divider"></span>
+            <div class="profile">
+                <img
+                    src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cGVvcGxlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+                    alt="">
+                <ul class="profile-link">
+                    <li><a href="#" id="logoutButton"><i class='bx bxs-log-out-circle'></i> Logout</a></li>
+                </ul>
+            </div>
+        </nav>
         `;
+    }
+
+    addLogoutEventListener() {
+        const logoutButton = this.querySelector('#logoutButton');
+        if (logoutButton) {
+            logoutButton.addEventListener('click', (event) => {
+                event.preventDefault();
+                this.logout();
+            });
+        }
+    }
+
+    logout() {
+        localStorage.removeItem('token'); 
+        alert('You have been logged out');
+        window.location.href = "/index.html"; 
     }
 }
 
